4bd0c03c6dd0f18fb57fc449c2d4897040bd47ab
joaogabriellyra <jgabriellyra@hotmail.com>
2024-09-18 23:43:15 -0300

    created the task schema

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -1,5 +1,8 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
 import TaskService from '../services/tasks'
 
 export default class TaskController {
   constructor(private taskService: TaskService) {}
+
+  async createTask(req: FastifyRequest, reply: FastifyReply) {}
 }
